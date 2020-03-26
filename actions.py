4bfe35c5b1696de9cff4b0b43b4f54537fb3944c
rshahab <ramin.shahab@glu.com>
2020-03-25 23:52:04 -0700

    update actions class

--- actions.py
+++ actions.py
@@ -4,24 +4,26 @@ from __future__ import unicode_literals
 
 from rasa_sdk import Action
 from rasa_sdk.events import SlotSet
-
+import requests
 
 class ActionWeather(Action):
     def name(self):
         return 'action_weather'
 
-    def run(self, dispatcher, tracker, domain):
-        from apixu.client import ApixuClient
-        api_key = "4375fd6915057370afea8398ddc7f7c2"
-        client = ApixuClient(api_key)
-
+    def run(self, dispatcher, tracker):
+        url = "http://api.weatherstack.com/current"
+        access_key = "4375fd6915057370afea8398ddc7f7c2"
         loc = tracker.get_slot('location')
-        current = client.current(q=loc)
-        city = current['location']['name']
-        condition = current['current']['condition']['text']
-        temperature_c = current['current']['temp_c']
-        humidity = current['current']['humidity']
-        wind_mph = current['current']['wind_mph']
+        params = {'access_key': access_key, 'query': loc}
+
+        r = requests.get(url=url, params=params)
+        data = r.json()
+
+        city = data['location']['name']
+        condition = data['current']['weather_descriptions']
+        temperature_c = data['current']['temperature']
+        humidity = data['current']['humidity']
+        wind_mph = data['current']['wind_speed']
 
         response = """It is currently {} in {} at the moment. The temperature is {} degrees, the humidity is {}% and the wind speed is {} mph.""".format(
             condition, city, temperature_c, humidity, wind_mph)
