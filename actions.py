cb365ae0110db129007f131f67452d5c3dd2632f
rshahab <ramin.shahab@glu.com>
2020-03-25 21:36:27 -0700

    updates actions class

--- actions.py
+++ actions.py
@@ -2,30 +2,29 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import unicode_literals
 
-from rasa_core_sdk import Action
-from rasa_core_sdk.events import SlotSet
+from rasa_sdk import Action
+from rasa_sdk.events import SlotSet
+
 
 class ActionWeather(Action):
-	def name(self):
-		return 'action_weather'
-		
-	def run(self, dispatcher, tracker, domain):
-		from apixu.client import ApixuClient
-		api_key = '...' #your apixu key
-		client = ApixuClient(api_key)
-		
-		loc = tracker.get_slot('location')
-		current = client.getcurrent(q=loc)
-		
-		country = current['location']['country']
-		city = current['location']['name']
-		condition = current['current']['condition']['text']
-		temperature_c = current['current']['temp_c']
-		humidity = current['current']['humidity']
-		wind_mph = current['current']['wind_mph']
+    def name(self):
+        return 'action_weather'
 
-		response = """It is currently {} in {} at the moment. The temperature is {} degrees, the humidity is {}% and the wind speed is {} mph.""".format(condition, city, temperature_c, humidity, wind_mph)
-						
-		dispatcher.utter_message(response)
-		return [SlotSet('location',loc)]
+    def run(self, dispatcher, tracker, domain):
+        from apixu.client import ApixuClient
+        api_key = "4375fd6915057370afea8398ddc7f7c2"
+        client = ApixuClient(api_key)
 
+        loc = tracker.get_slot('location')
+        current = client.current(q=loc)
+        city = current['location']['name']
+        condition = current['current']['condition']['text']
+        temperature_c = current['current']['temp_c']
+        humidity = current['current']['humidity']
+        wind_mph = current['current']['wind_mph']
+
+        response = """It is currently {} in {} at the moment. The temperature is {} degrees, the humidity is {}% and the wind speed is {} mph.""".format(
+            condition, city, temperature_c, humidity, wind_mph)
+
+        dispatcher.utter_message(response)
+        return [SlotSet('location', loc)]
