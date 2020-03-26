02911c8897d58263d89811357ae5275cab5284a5
rshahab <ramin.shahab@glu.com>
2020-03-26 00:17:42 -0700

    update nlu model file

--- nlu-model.py
+++ nlu-model.py
@@ -1,13 +1,18 @@
-from rasa import nlu
+from rasa_nlu.training_data import load_data
+from rasa_nlu.config import RasaNLUModelConfig
+from rasa_nlu.model import Trainer
+from rasa_nlu import config
+from rasa_nlu.model import Metadata, Interpreter
+
 
 def train_nlu(data, config, model_dir):
     training_data = nlu.load_data(data)
     # Pass configs to the NLU training module
-    # trainer = trainer(RasaNLUModelConfig))
+    trainer = Trainer(RasaNLUModelConfig)
     # where the training happens
-    # trainer.train(training_data)
+    trainer.train(training_data)
     # directory of model name
-    # model_directory = trainer.persist(model_dir, fixed_model_name="weathernlu")
+    model_directory = trainer.persist(model_dir, fixed_model_name="weathernlu")
 
 
 if __name__ == 'main':
